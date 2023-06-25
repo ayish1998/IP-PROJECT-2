e1d5958c23c2b4011ee40aafeb833dde18c6be3a
Ayishatu Saeed <91335764+ayish1998@users.noreply.github.com>
2023-06-25 18:36:43 +0400

    News feed for the website

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,22 @@
-fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "quotes15.p.rapidapi.com",
-        "x-rapidapi-key": "d04a271587msh1c8c70737cc9b87p1da6c7jsnf560bf3fe7c6"
+const cards = document.getElementsByClassName('card');
+            const cardArray = Array.from(cards);
 
-    }
-})
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-        console.log(response.content);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-
-
-
-
-
+            cardArray.forEach(card => {
+                card.addEventListener('click', () => {
+                    fetch('https://climate-news-feed.p.rapidapi.com/page/1?limit=10', {
+                        method: 'GET',
+                        headers: {
+                            'X-RapidAPI-Key': 'd04a271587msh1c8c70737cc9b87p1da6c7jsnf560bf3fe7c6',
+                            'X-RapidAPI-Host': 'climate-news-feed.p.rapidapi.com'
+                        }
+                    })
+                        .then(response => response.json())
+                        .then(response => {
+                            console.log(response);
+                            console.log(response.content);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+                });
+            });
